a6af23f0624218eefdc202201346a1b82c9c1aa4
Ernie-Su <32356926+Ernie-Su@users.noreply.github.com>
2021-06-06 15:29:30 +0800

    add Face Detection without Server
    This is adapted based on the code Martin uploaded. It's a local face detection without Wi-Fi connection and HTML interface. User can change the camera mode using Serial Input.

---
+++ FaceDetectionWithoutServer/FaceDetectionWithoutServer.ino
@@ -0,0 +1,319 @@
+#include "esp_timer.h"
+#include "esp_camera.h"
+#include "camera_index.h"
+#include "fd_forward.h"
+#include "fr_forward.h"
+#include "fr_flash.h"
+#include "camera_pins.h"
+
+#define ENROLL_CONFIRM_TIMES 5
+#define FACE_ID_SAVE_NUMBER 7
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_ESP_EYE
+#define CAMERA_MODEL_AI_THINKER
+
+// Change this line based on your WiFi connection
+/*
+const char ssid[] = "";
+const char password[] = "";
+*/
+
+camera_fb_t * fb = NULL;
+
+long current_millis;
+long last_detected_millis = 0;
+
+bool face_recognised = false;
+
+void app_facenet_main();
+
+typedef struct
+{
+  uint8_t *image;
+  box_array_t *net_boxes;
+  dl_matrix3d_t *face_id;
+} http_img_process_result;
+
+static inline mtmn_config_t app_mtmn_config()
+{
+  mtmn_config_t mtmn_config = {0};
+  mtmn_config.type = FAST;
+  mtmn_config.min_face = 80;
+  mtmn_config.pyramid = 0.707;
+  mtmn_config.pyramid_times = 4;
+  mtmn_config.p_threshold.score = 0.6;
+  mtmn_config.p_threshold.nms = 0.7;
+  mtmn_config.p_threshold.candidate_number = 20;
+  mtmn_config.r_threshold.score = 0.7;
+  mtmn_config.r_threshold.nms = 0.7;
+  mtmn_config.r_threshold.candidate_number = 10;
+  mtmn_config.o_threshold.score = 0.7;
+  mtmn_config.o_threshold.nms = 0.7;
+  mtmn_config.o_threshold.candidate_number = 1;
+  return mtmn_config;
+}
+mtmn_config_t mtmn_config = app_mtmn_config();
+
+face_id_name_list st_face_list;
+static dl_matrix3du_t *aligned_face = NULL;
+
+typedef enum
+{
+  START_STREAM,
+  START_DETECT,
+  SHOW_FACES,
+  START_RECOGNITION,
+  START_ENROLL,
+  ENROLL_COMPLETE,
+  DELETE_ALL,
+} en_fsm_state;
+en_fsm_state g_state=START_RECOGNITION;
+
+typedef struct
+{
+  char enroll_name[ENROLL_NAME_LEN];
+} httpd_resp_value;
+
+httpd_resp_value st_name;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  /*
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  */
+  
+  app_facenet_main();
+  
+  /*
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+  */
+}
+
+void app_facenet_main()
+{
+  face_id_name_init(&st_face_list, FACE_ID_SAVE_NUMBER, ENROLL_CONFIRM_TIMES);
+  aligned_face = dl_matrix3du_alloc(1, FACE_WIDTH, FACE_HEIGHT, 3);
+  read_face_id_from_flash_with_name(&st_face_list);
+}
+
+static inline int do_enrollment(face_id_name_list *face_list, dl_matrix3d_t *new_id)
+{
+  ESP_LOGD(TAG, "START ENROLLING");
+  int left_sample_face = enroll_face_id_to_flash_with_name(face_list, new_id, st_name.enroll_name);
+  ESP_LOGD(TAG, "Face ID %s Enrollment: Sample %d",
+           st_name.enroll_name,
+           ENROLL_CONFIRM_TIMES - left_sample_face);
+  return left_sample_face;
+}
+
+static esp_err_t send_face_list()
+{
+  Serial.println("delete_faces");
+  face_id_node *head = st_face_list.head;
+  char add_face[64];
+  for (int i = 0; i < st_face_list.count; i++) // loop current faces
+  {
+    sprintf(add_face, "listface:%s", head->id_name);
+    Serial.println(add_face);
+    head = head->next;
+  }
+}
+
+static esp_err_t delete_all_faces()
+{
+  delete_face_all_in_flash_with_name(&st_face_list);
+  Serial.println("delete_faces");
+}
+
+// try to use Serial input to control the camera mode
+void Cam_Mode_Select() {
+  if (Serial.available() > 1) {
+    char input_char = Serial.read();
+    Serial.print("Pressed ");
+    Serial.print(input_char);
+    
+    switch(input_char){
+      case 's':
+      case 'S':
+        g_state = START_STREAM;
+        Serial.println(", START_STREAM");
+        break;
+      
+      case 'd':
+      case 'D':
+        g_state = START_DETECT;
+        Serial.println(", START_DETECT");
+        break;
+        
+      case 'r':
+      case 'R':
+        g_state = START_RECOGNITION;
+        Serial.println(", START_RECOGNITION");
+        break;
+
+      case 'e':
+      case 'E':
+        g_state = START_ENROLL;
+        Serial.println(", START_ENROLL");
+        break;
+      
+      default:
+        Serial.println(", Try again");
+        break;
+    }
+  }
+}
+
+void loop() {
+  dl_matrix3du_t *image_matrix = dl_matrix3du_alloc(1, 320, 240, 3);
+  http_img_process_result out_res = {0};
+  out_res.image = image_matrix->item;
+
+  send_face_list();
+  Serial.println("STREAMING"); 
+  
+  while (1) {    
+    fb = esp_camera_fb_get();
+
+    // try to use Serial input to control the camera mode
+    Cam_Mode_Select();
+
+    if (g_state == START_DETECT || g_state == START_ENROLL || g_state == START_RECOGNITION)
+    {
+      out_res.net_boxes = NULL;
+      out_res.face_id = NULL;
+
+      fmt2rgb888(fb->buf, fb->len, fb->format, out_res.image);
+
+      out_res.net_boxes = face_detect(image_matrix, &mtmn_config);
+
+      if (out_res.net_boxes)
+      {
+        if (align_face(out_res.net_boxes, image_matrix, aligned_face) == ESP_OK)
+        {
+
+          out_res.face_id = get_face_id(aligned_face);
+          last_detected_millis = millis();
+          if (g_state == START_DETECT) {
+            Serial.println("FACE DETECTED");
+          }
+
+          if (g_state == START_ENROLL)
+          {
+            int left_sample_face = do_enrollment(&st_face_list, out_res.face_id);
+            char enrolling_message[64];
+            sprintf(enrolling_message, "SAMPLE NUMBER %d FOR %s", ENROLL_CONFIRM_TIMES - left_sample_face, st_name.enroll_name);
+            Serial.println(enrolling_message);
+            if (left_sample_face == 0)
+            {
+              ESP_LOGI(TAG, "Enrolled Face ID: %s", st_face_list.tail->id_name);
+              g_state = START_STREAM;
+              char captured_message[64];
+              sprintf(captured_message, "FACE CAPTURED FOR %s", st_face_list.tail->id_name);
+              Serial.println(captured_message);
+              send_face_list();
+
+            }
+          }
+          
+          // RECOGNITION button is pressed and face are detected
+          if (g_state == START_RECOGNITION  && (st_face_list.count > 0))
+          {
+            face_id_node *f = recognize_face_with_name(&st_face_list, out_res.face_id);
+            if (f)
+            {
+              char recognised_message[64];
+              sprintf(recognised_message, "WELCOME %s", f->id_name);
+              Serial.println(recognised_message);
+            }
+            else
+            {
+              Serial.println("FACE NOT RECOGNISED");
+            }
+          }
+          dl_matrix3d_free(out_res.face_id);
+        }
+
+      }
+      else
+      {
+        if (g_state != START_DETECT) {
+          Serial.println("NO FACE DETECTED");
+        }
+      }
+
+      if (g_state == START_DETECT && millis() - last_detected_millis > 500) { // Detecting but no face detected
+        Serial.println("DETECTING");
+      }
+
+    }
+    
+    esp_camera_fb_return(fb);
+    fb = NULL;
+  }
+}
